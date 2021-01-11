7317cc360f1f45a4dbbc9fe46ea7d81a43b9d55b
Trevor Mearns <trevor@trevormearns.com>
2021-01-11 09:56:42 +0900

    first commit

--- src/components/ExpenseInput.js
+++ src/components/ExpenseInput.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ExpenseButton from "./ExpenseButton";
-import ExpenseItem from "./ExpenseItem";
+import React from 'react';
+import ExpenseButton from './ExpenseButton';
+import ExpenseItem from './ExpenseItem';
 
 class ExpenseInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       expenses: [],
-      date: "",
-      type: "",
-      merchant: "",
-      amount: "",
+      date: '',
+      paymentType: '',
+      merchant: '',
+      item: '',
+      amount: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -19,24 +20,24 @@ class ExpenseInput extends React.Component {
   }
 
   componentDidMount() {
-    const storage = JSON.parse(localStorage.getItem("expenseItems"));
+    const storage = JSON.parse(localStorage.getItem('expenseItems'));
 
     storage &&
       this.setState({
-        expenses: storage,
+        expenses: storage
       });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.expenses !== this.state.expenses) {
       localStorage.clear();
-      localStorage.setItem("expenseItems", JSON.stringify(this.state.expenses));
+      localStorage.setItem('expenseItems', JSON.stringify(this.state.expenses));
     }
   }
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value,
+      [event.target.name]: event.target.value
     });
   }
 
@@ -48,9 +49,10 @@ class ExpenseInput extends React.Component {
     expenseArray.push({
       id: randomId,
       date: event.target.date.value,
-      type: event.target.type.value,
+      paymentType: event.target.type.value,
       merchant: event.target.merchant.value,
-      amount: event.target.amount.value,
+      item: event.target.item.value,
+      amount: event.target.amount.value
     });
 
     expenseArray.sort((a, b) => {
@@ -62,15 +64,14 @@ class ExpenseInput extends React.Component {
     this.setState({
       ...this.state,
       expenses: expenseArray,
-      date: "",
-      type: "",
-      merchant: "",
-      amount: "",
+      date: '',
+      paymentType: '',
+      merchant: '',
+      item: '',
+      amount: ''
     });
 
-    localStorage.setItem("expenseItems", JSON.stringify(this.state.expenses));
-
-    alert("Expense for " + event.target.merchant.value + " added");
+    localStorage.setItem('expenseItems', JSON.stringify(this.state.expenses));
   }
 
   handleDeleteExpense(event) {
@@ -83,7 +84,7 @@ class ExpenseInput extends React.Component {
 
     this.setState({
       ...this.state,
-      expenses: deleteExpenseFromArray,
+      expenses: deleteExpenseFromArray
     });
   }
 
@@ -98,61 +99,72 @@ class ExpenseInput extends React.Component {
     ));
 
     return (
-      <div className="expense-body-container">
-        <div className="expense-input-container">
+      <div className='expense-body-container'>
+        <div className='expense-input-container'>
           <form onSubmit={this.handleSubmit}>
-            <div className="form-row expense-input-content">
-              <div className="form-group col-sm-3">
-                <label className="col col-sm-.5 col-form-label">Date:</label>
+            <div className='form-row expense-input-content'>
+              <div className='form-group col-sm-3'>
+                <label className='col col-sm-.5 col-form-label'>Date:</label>
                 <input
-                  className="form-control form-control-lg"
-                  type="date"
-                  id="date"
-                  name="date"
+                  className='form-control form-control-lg'
+                  type='date'
+                  id='date'
+                  name='date'
                   value={this.state.date}
                   onChange={this.handleChange}
                   required
                 ></input>
               </div>
 
-              <div className="form-group col-sm-3">
-                <label className="col col-sm-.5 col-form-label">Type:</label>
+              <div className='form-group col-sm-3'>
+                <label className='col col-sm-.5 col-form-label'>Type:</label>
                 <select
-                  className="col col-sm-2.5 form-control-lg"
-                  id="type"
-                  name="type"
-                  value={this.state.type}
+                  className='col col-sm-2.5 form-control-lg'
+                  id='type'
+                  name='type'
+                  value={this.state.paymentType}
                   onChange={this.handleChange}
                   required
                 >
-                  <option value="Cash">Cash</option>
-                  <option value="Credit">Credit</option>
-                  <option value="Crypto">Crypto</option>
+                  <option value='Cash'>Cash</option>
+                  <option value='Credit'>Credit</option>
+                  <option value='Crypto'>Crypto</option>
                 </select>
               </div>
 
-              <div className="form-group col-sm-3">
-                <label className="col col-sm-.5 col-form-label">
+              <div className='form-group col-sm-3'>
+                <label className='col col-sm-.5 col-form-label'>
                   Merchant:
                 </label>
                 <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  id="merchant"
-                  name="merchant"
+                  className='form-control form-control-lg'
+                  type='text'
+                  id='merchant'
+                  name='merchant'
                   value={this.state.merchant}
                   onChange={this.handleChange}
                   required
                 ></input>
               </div>
-
-              <div className="form-group col-sm-3">
-                <label className="col col-sm-.5 col-form-label">Amount:</label>
+              <div className='form-group col-sm-3'>
+                <label className='col col-sm-.5 col-form-label'>Item:</label>
                 <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  id="amount"
-                  name="amount"
+                  className='form-control form-control-lg'
+                  type='text'
+                  id='item'
+                  name='item'
+                  value={this.state.item}
+                  onChange={this.handleChange}
+                  required
+                ></input>
+              </div>
+              <div className='form-group col-sm-3'>
+                <label className='col col-sm-.5 col-form-label'>Amount:</label>
+                <input
+                  className='form-control form-control-lg'
+                  type='text'
+                  id='amount'
+                  name='amount'
                   value={this.state.amount}
                   onChange={this.handleChange}
                   required
@@ -163,14 +175,15 @@ class ExpenseInput extends React.Component {
             <ExpenseButton />
           </form>
         </div>
-        <div className="row expense-labels">
-          <div className="col col-sm-3 expense-labels-item">Date</div>
-          <div className="col col-sm-2 expense-labels-item">Type</div>
-          <div className="col col-sm-3 expense-labels-item">Merchant</div>
-          <div className="col col-sm-2 expense-labels-item">Amount</div>
-          <div className="col col-sm-1 expense-labels-item">Delete</div>
+        <div className='row expense-labels'>
+          <div className='col col-sm-3 expense-labels-item'>Date</div>
+          <div className='col col-sm-2 expense-labels-item'>Type</div>
+          <div className='col col-sm-3 expense-labels-item'>Merchant</div>
+          <div className='col col-sm-3 expense-labels-item'>Item</div>
+          <div className='col col-sm-2 expense-labels-item'>Amount</div>
+          <div className='col col-sm-1 expense-labels-item'>Delete</div>
         </div>
-        <div className="expense-item">{expenseItems}</div>
+        <div className='expense-item'>{expenseItems}</div>
       </div>
     );
   }
